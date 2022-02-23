ae627d5cc1524c5b9b47cae6c9d79f53e94822bc
vws21 <79153103+vws21@users.noreply.github.com>
2022-02-23 18:21:44 -0500

    Update simple.py

--- simple.py
+++ simple.py
@@ -1,3 +1,3 @@
-name=input("What is your name?")
-age=input("What is your age?")
-print("Your name is " + name + " and you are " + age + "years old.")
+name=input("What is your name?") # retrieve name from user
+age=input("What is your age?") # retrieve age from user
+print("Your name is " + name + " and you are " + age + "years old.") # prints a sentence with user's inputted name and age
